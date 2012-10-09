bde1e311ab2210d14c5220bef94454a29856d4fd
mgsnova <github@mgsnova.de>
2012-10-09 11:48:11 +0200

    add osx initial script

---
+++ osx/init.sh
@@ -0,0 +1,15 @@
+# initial stuff for setting up osx
+
+# install command line tools for xcode
+# goto https://developer.apple.com/downloads
+
+# homebrew + packages
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+brew doctor
+brew install ack git redis memcached wget tmux tkdiff readline cmake
+brew tap homebrew/dupes
+brew install autoconf automake apple-gcc42
+
+# rvm
+curl -L https://get.rvm.io | bash -s stable
+rvm requirements
