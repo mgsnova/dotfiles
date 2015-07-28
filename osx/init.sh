3861480396e7b6ea91d993d72fe1df3645d6f3f9
mgsnova <github@mgsnova.de>
2015-07-28 15:15:44 +0200

    update osx settings

--- osx/init.sh
+++ osx/init.sh
@@ -6,7 +6,7 @@
 # homebrew + packages
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 brew doctor
-brew install ack git redis memcached wget tmux tkdiff readline cmake
+brew install ack git redis wget tmux tkdiff readline cmake cloc zsh tree
 brew tap homebrew/dupes
 brew install autoconf automake apple-gcc42
 
@@ -15,5 +15,11 @@ curl -L https://get.rvm.io | bash -s stable
 rvm requirements
 
 # osx settings
-defaults write com.apple.dock expose-animation-duration -float 0.10 && killall Dock
-defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+chflags nohidden ~/Library
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.finder EmptyTrashSecurely -bool true
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain KeyRepeat -int 0
+killall Dock Finder
